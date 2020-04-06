60f2534b65673280c6683e3cffc2682c2e0929c0
mackenzie martin <63261641+littlesunsh9@users.noreply.github.com>
2020-04-06 18:41:21 -0400

    Add files via upload
    Adjustments to the crop file in order to account for multiple images at one time were added. A For loop, allowing the program to run through a list of images, was created.

--- crop.r
+++ crop.r
@@ -1,14 +1,21 @@
 # Raster calcultion to crop Geotiff, also pulls XML metadata: reflectance coefficient (ps:reflectanceCoefficient)
-
+library(rgdal)
 library(raster)
 library(rgeos)
 library(XML)
 library(methods)
 
 # remember to set working directory if needed:
-setwd("/Volumes/LaCie2big/widthflowproject/r_test")
+setwd("/Users/littlesunsh9/Documents/planet_order_181828/")
+#Lists for necessary files
+#Image list
+i <- list.files("/Users/littlesunsh9/Documents/planet_order_181828/", pattern = "*AnalyticMS.tif$", full.names = TRUE, recursive = TRUE, ignore.case=TRUE, include.dirs = TRUE)
+#Metadata List
+c <- list.files("/Users/littlesunsh9/Documents/planet_order_181828/", pattern = "*AnalyticMS_metadata.xml$", full.names = TRUE, recursive = TRUE, ignore.case=TRUE, include.dirs = TRUE)
 
-fn <- "./20180212_155035_0f3c_3B_AnalyticMS_metadata.xml"
+for (q in 1:(length(i))){
+
+fn <- c[q]
 fl <- xmlParse(fn)
 rc <- setNames(xmlToDataFrame(node=getNodeSet(fl, "//ps:EarthObservation/gml:resultOf/ps:EarthObservationResult/ps:bandSpecificMetadata/ps:reflectanceCoefficient")),"reflectanceCoefficient")
 dm <- as.matrix(rc)
@@ -20,7 +27,7 @@ rc2 <- as.numeric(dm[2]) # Green
 rc4 <- as.numeric(dm[4]) # NIR
 
 # Import raster image
-fn <- "./20180212_155035_0f3c_3B_AnalyticMS.tif"
+fn <- i[q]
 pic <- stack(fn)
 # set extent from QGIS analysis:
 # extent format (xmin,xmax,ymin,ymax)
@@ -38,11 +45,18 @@ ndwi <- ((rc2*r[[2]]) - (rc4*r[[4]])) / ((rc2*r[[2]]) + (rc4*r[[4]]))
 # To view, during development
 #plot(ndwi)
 
+p <- strsplit(i[q], "_3B_AnalyticMS.tif")
+r <- strsplit(p[[1]], "/")
+lr <- tolower(r[[1]])
+len <- length(lr)
+root <- lr[[len]]
+
 # To export NDWI as a new file
 writeRaster(x = ndwi,
-            filename="test.tif",
+            filename= paste(root, "ndwi.tif", sep="."),
             format = "GTiff", # save as a tif
-            datatype='INT2S', # save as a INTEGER rather than a float
+            # save as a FLOAT if not default, not integer
             overwrite = TRUE)  # OPTIONAL - be careful. This will OVERWRITE previous files.
 #attempting the following crop procedure https://gis.stackexchange.com/questions/229356/crop-a-raster-file-in-r
 #attempting the following NDVI as NDWI procedure https://www.earthdatascience.org/courses/earth-analytics/multispectral-remote-sensing-data/vegetation-indices-NDVI-in-R/
+}

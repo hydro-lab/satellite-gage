ddd53cca58c50bf8ba61e80e82ad34bff147d6fb
mackenzie martin <63261641+littlesunsh9@users.noreply.github.com>
2020-05-15 10:18:43 -0400

    Add files via upload

--- master.R
+++ master.R
@@ -61,7 +61,7 @@ for (q in 1:(length(i))){
               overwrite = TRUE)  # OPTIONAL - be careful. This will OVERWRITE previous files.
   #attempting the following crop procedure https://gis.stackexchange.com/questions/229356/crop-a-raster-file-in-r
   #attempting the following NDVI as NDWI procedure https://www.earthdatascience.org/courses/earth-analytics/multispectral-remote-sensing-data/vegetation-indices-NDVI-in-R/
-  
+
   # LOOP ENDS HERE !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
 
@@ -162,3 +162,54 @@ write.table(ex, file = "wateredge.csv", append = TRUE, sep = ",", dec = ".", col
 # make sure input and output filenames are coded correctly
 # https://cran.r-project.org/doc/manuals/R-intro.html#Invoking-R-from-the-command-line
 
+setwd("/Users/littlesunsh9/Documents/planet_order_181828/")
+
+cndwi <- list.files("/Users/littlesunsh9/Documents/planet_order_181828/", pattern = "*.cndwi.tif$", full.names = TRUE, recursive = TRUE, ignore.case=TRUE, include.dirs = TRUE)
+for (q in 1:(length(cndwi))){
+fn <- cndwi[q]
+ndwia <- stack(fn)
+e <- as(extent(609555.5999,609709.1999,4507753.099,4507867.5999 ), 'SpatialPolygons')
+crs(e) <- "+proj=utm +zone=17 +datum=WGS84"
+#RDB=(609589.376, 4507801.407)
+#LDB=(609634.607, 4507831.586)
+x1 <- (609589.376)
+x2 <- (609634.607)
+y1 <- (4507801.407)
+y2 <- (4507831.586)
+m <- (y2-y1)/(x2-x1)
+#this next part will rely on UTM (the coordinates are in meters)
+r <- (0.1)
+#r is the step size of each point along our width
+t <- sqrt(((x2-x1)^2)+ ((y2-y1)^2))
+f <- ceiling(t/r)
+pointers <- array(999.999, dim = c(f,2))
+pointers[1,1] <- x1
+pointers[1,2] <- y1
+for (i in 2:f){
+  a <- 1
+  b <- (-2)*pointers[i-1,1]
+  c <- (pointers[i-1,1]^2) - (r^2)/((m^2)+1)
+  pointers[i,1] <- ((-b)+(sqrt((b^2)-4*a*c)))/(2*a)
+  pointers[i,2] <- ((pointers[i-1,2])+(m*((pointers[i,1])-(pointers[i-1,1]))))
+}
+
+#Check:
+#x <- c(x1,x2)
+#y <- c(y1,y2)
+#plot(x,y)
+
+P <- SpatialPoints(pointers)
+
+alng <- extract(ndwia, P, method='simple')
+plot(alng)
+
+p <- strsplit(cndwi[q], "cndwi.tif")
+r <- strsplit(p[[1]], "/")
+lr <- tolower(r[[1]])
+len <- length(lr)
+root <- lr[[len]]
+
+# To export table as a new file
+write.table(alng, file = paste(root, "distwidth.csv", sep="."), append = TRUE, sep = ",", dec = ".", col.names = FALSE)
+
+}
